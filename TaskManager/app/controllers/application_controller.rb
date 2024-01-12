d4a34d32a65e6d5f4104c26d9d6520a0f5b00336
igoorsimoess <igor.s.simoes@gmail.com>
2024-01-12 15:59:57 -0300

    Add Authentication validation

--- TaskManager/app/controllers/application_controller.rb
+++ TaskManager/app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
-class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+class ApplicationController < ActionController::API
+    before_action :authorized
+
+    # this will build the token
+
+    def encode_token(payload)
+        secret_key = Rails.application.credentials.secret_key_base
+        JWT.encode(payload, secret_key) 
+    end
+
+    def decoded_token
+        secret_key = Rails.application.credentials.secret_key_base
+        header = request.headers['Authorization']
+        if header
+            token = header.split(" ")[1] # splits the string and takes only the actual authorization header
+            begin
+                JWT.decode(token, secret_key)
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user 
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def authorized
+        unless !!current_user
+            render json: { message: 'Please log in' }, status: :unauthorized
+        end
+    end
 end
--- TaskManager/config/routes.rb
+++ TaskManager/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
+  post "/users", to: "users#create"
+  get "/me", to: "users#me"
+  post "/auth/login", to: "auth#login"
 end
